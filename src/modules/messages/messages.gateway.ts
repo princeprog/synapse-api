0a9488a4bc23ce92e38b37871738047f3c0d94ad
Al Prince Llavan <alprincellavan2019@gmail.com>
2026-04-04 16:00:47 +0800

    Added message reactions

--- src/modules/messages/messages.gateway.ts
+++ src/modules/messages/messages.gateway.ts
@@ -57,6 +57,9 @@ type ThreadPayload = ChannelPayload & {
   namespace: '/messages',
   cors: {
   cors:{
+@WebSocketGateway({
+  namespace: '/messages',
+  cors: {
     origin: ['http://localhost:3000', 'http://localhost:3001'],
     credentials: true,
   },
